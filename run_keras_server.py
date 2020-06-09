e61a6b5c816550254ebbcc7799107115bfc9af09
arifmedamine <mohamedamine.arif@esprit.tn>
2020-06-10 00:01:53 +0100

    first commit

---
+++ run_keras_server.py
@@ -0,0 +1,109 @@
+# USAGE
+# python classify.py --model pokedex.model --labelbin lb.pickle --image examples/charmander_counter.png
+
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import pickle
+import cv2
+import os
+import flask
+from flask import render_template, request, jsonify
+import pandas as pd
+import traceback
+from werkzeug import secure_filename
+import sys
+from skimage import io
+from PIL import Image
+
+# initialize our Flask application and the Keras model
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+   return "Client Signature Prediction"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if flask.request.method == "POST":
+        f = request.files["file"]
+        f_name = secure_filename(f.filename)
+        f.save(f_name)
+        image = cv2.imread(f_name)
+        image = cv2.resize(image, (64, 64))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        my_model = load_model('signature.model')
+        my_lb = pickle.loads(open('lb.pickle', "rb").read())
+        proba = my_model.predict(image)[0]
+        idx = np.argmax(proba)
+        label = my_lb.classes_[idx]
+        filename = f_name[f_name.rfind(os.path.sep) + 1:]
+        correct = "correct" if filename.rfind(label) != -1 else "correct"
+        label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
+
+        result = "[INFO] {}".format(label)
+
+        print(f'Image predicted as: {result}')
+
+        return f'Image predicted as: {result}'
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
